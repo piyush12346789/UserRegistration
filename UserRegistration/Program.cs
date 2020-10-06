14773186875af13a796413f9f14cd3fc9920cc79
piyush12346789 <piyushgupta024@gmail.com>
2020-10-06 13:28:29 +0530

    [Piyush] Add: First name validation.

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ValidateUserRegistration validateUserRegistration = new ValidateUserRegistration();
+            Console.WriteLine("Enter your first name : ");
+            string firstName = Console.ReadLine();
+            bool fNameResult = validateUserRegistration.ValidateFirstName(firstName);
+            validateUserRegistration.PrintResult(fNameResult);
+        }
+    }
+}
---
+++ UserRegistration/ValidateUserRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+namespace UserRegistration
+{
+    class ValidateUserRegistration
+    {
+        private static string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        public bool ValidateFirstName(string FN)
+        {
+            return Regex.IsMatch(FN, Regex_FirstName);
+        }
+        public void PrintResult(bool result)
+        {
+            if (result)
+            {
+                Console.WriteLine("Valid.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid.");
+            }
+        }
+    }
+}
