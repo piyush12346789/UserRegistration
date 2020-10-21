4ce1b15b721495dc91852bbeba080e72345a4b91
piyush12346789 <piyushgupta024@gmail.com>
2020-10-21 18:58:10 +0530

    [Piyush] Refactor: Refactored the code to use lambda function to validate user entry.

--- UserRegistration/ValidateUserRegistration.cs
+++ UserRegistration/ValidateUserRegistration.cs
@@ -6,6 +6,12 @@ namespace UserRegistration
 {
     public class ValidateUserRegistration
     {
+        private static string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
+        private static string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
+        private static string Regex_Email = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]{0,})*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
+        private static string Regex_MobileNo = "^[1-9]{1}[0-9]{1,2}[ ][1-9]{1}[0-9]{9}$";
+        private static string Regex_Password = "^(?=.*[0-9])(?=.*[A-Z])(?=[^!@#$%&+-.]*[!@#$%&+-.][^!@#$%&+-.]*$)[\\S]{8,}$";
+        private static string Regex_Email2 = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]{0,})*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
         private List<string> sampleMails = new List<string>()
         {
             "abc@yahoo.com",
@@ -18,79 +24,12 @@ namespace UserRegistration
             "abc@gmail.com.com",
             "abc+100@gmail.com"
         };
-        private static string Regex_FirstName = "^[A-Z]{1}[a-z]{2,}$";
-        private static string Regex_LastName = "^[A-Z]{1}[a-z]{2,}$";
-        private static string Regex_Email = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]{0,})*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";
-        private static string Regex_MobileNo = "^[1-9]{1}[0-9]{1,2}[ ][1-9]{1}[0-9]{9}$";
-        private static string Regex_Password = "^(?=.*[0-9])(?=.*[A-Z])(?=[^!@#$%&+-.]*[!@#$%&+-.][^!@#$%&+-.]*$)[\\S]{8,}$";
-        private static string Regex_Email2 = "^[0-9a-zA-Z]{1,}([._+-][0-9a-zA-Z]{0,})*[@][0-9a-zA-Z]{1,}.[a-zA-Z]{2,3}(.[a-zA-Z]{2,3})?$";        
-        public bool ValidateFirstName(string FN)
-        {
-            
-            if(Regex.IsMatch(FN, Regex_FirstName))
-            {
-                return true;
-            }
-            else
-            {
-                throw new URCustomException(URCustomException.ExceptionType.INVALID_FIRST_NAME, "Invalid first name.");
-            }
-        }
-        public bool ValidateLastName(string LN)
-        {
-            if (Regex.IsMatch(LN, Regex_LastName))
-            {
-                return true;
-            }
-            else
-            {
-                throw new URCustomException(URCustomException.ExceptionType.INVALID_LAST_NAME, "Invalid last name.");
-            }
-        }
-        public bool ValidateEmail(string Email)
-        {
-            if(Regex.IsMatch(Email, Regex_Email))
-            {
-                return true;
-            }
-            else
-            {
-                throw new URCustomException(URCustomException.ExceptionType.INVALID_EMAIL, "Invalid email address.");
-            }
-        }
-        public bool ValidateMobileNo(string MobileNo)
-        {
-            if(Regex.IsMatch(MobileNo, Regex_MobileNo))
-            {
-                return true;
-            }
-            else
-            {
-                throw new URCustomException(URCustomException.ExceptionType.INVALID_MOBILENO, "Invalid mobile no.");
-            }
-        }
-        public bool ValidatePassword(string Password)
-        {
-            if(Regex.IsMatch(Password, Regex_Password))
-            {
-                return true;
-            }
-            else
-            {
-                throw new URCustomException(URCustomException.ExceptionType.INVALID_PASSWORD, "Invalid password.");
-            }
-        }
-        public bool ValidateEmail2(string email)
-        {
-            if(Regex.IsMatch(email, Regex_Email2))
-            {
-                return true;
-            }
-            else
-            {
-                throw new URCustomException(URCustomException.ExceptionType.INVALID_EMAIL, "Invalid email address.");
-            }
-        }
+        public bool ValidateFirstName(string FN) => (Regex.IsMatch(FN, Regex_FirstName));
+        public bool ValidateLastName(string LN) => (Regex.IsMatch(LN, Regex_LastName));
+        public bool ValidateEmail(string Email) => (Regex.IsMatch(Email, Regex_Email));
+        public bool ValidateMobileNo(string MobileNo) => (Regex.IsMatch(MobileNo, Regex_MobileNo));
+        public bool ValidatePassword(string Password) => (Regex.IsMatch(Password, Regex_Password));
+        public bool ValidateEmail2(string email) => (Regex.IsMatch(email, Regex_Email2));
         public void PrintResult(bool result)
         {
             if (result)
--- UserRegistrationTesting/UnitTest1.cs
+++ UserRegistrationTesting/UnitTest1.cs
@@ -1,7 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using UserRegistration;
-
 namespace UserRegistrationTestings
 {
     [TestClass]
@@ -62,92 +61,6 @@ namespace UserRegistrationTestings
             bool result = user.ValidatePassword(password);
             //Assert
             Assert.IsTrue(result);
-        }
-        //Sad Test Cases(Test Cases Fail The Entry)
-        [TestMethod]
-        public void GivenUserFistName_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_FirstName()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string firstName = "piyush";
-                //Act
-                bool result = user.ValidateFirstName(firstName);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid first name.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserLastName_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_LastName()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string lastName = "gupta";
-                //Act
-                bool result = user.ValidateLastName(lastName);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid last name.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserEmail_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_Email()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string email = "piyushguptagmail.com";
-                //Act
-                bool result = user.ValidateEmail(email);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid email address.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserMobileNumber_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_MobileNo()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string mobileNumber = "11 1123453214";
-                //Act
-                bool result = user.ValidateMobileNo(mobileNumber);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid mobile number.", e.Message);
-            }
-        }
-        [TestMethod]
-        public void GivenUserPassword_WhenValidate_Should_Throw_URCustomException_Indicating_Invalid_Password()
-        {
-            try
-            {
-                //Arrange
-                ValidateUserRegistration user = new ValidateUserRegistration();
-                string password = "pqwert@123";
-                //Act
-                bool result = user.ValidatePassword(password);
-            }
-            catch (URCustomException e)
-            {
-                //Assert
-                Assert.AreEqual("Invalid password.", e.Message);
-            }
         }
         [TestMethod]
         [DataRow("abc@yahoo.com")]
